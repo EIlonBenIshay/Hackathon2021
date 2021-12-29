8c70ec0a91faa4d2ef5d320acc09e4a2332c8df2
Eilon Ben-Ishay <eilon950@gmail.com>
2021-12-29 14:05:13 +0200

    local host tests

--- Server.py
+++ Server.py
@@ -124,7 +124,7 @@ def UDPInitConnection(port):
 def TCPInitConnection(port):
     host = gethostname()
     sock = socket(AF_INET, SOCK_STREAM)
-    server_address = (host, port)
+    server_address = ("127.0.0.1", port)
     try:
         sock.bind(server_address)
     except:

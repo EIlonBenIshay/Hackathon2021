6cc5049a7f51547e4e7a67638e3f33017861a5d9
benfarin <63962531+benfarin@users.noreply.github.com>
2021-12-29 22:47:50 +0200

    final_niv

--- Client.py
+++ Client.py
@@ -1,223 +1,104 @@
-from socket import *
-from struct import *
+import socket
+import struct
+from threading import Thread
 import time
-import threading
-import random
+import getch
 
-#test
 Red = "\033[31;1m"
 Green = "\033[32;1m"
 Yellow = "\033[33;1m"
 Blue = "\033[34;1m"
 End = "\033[0;1m"
-#define some global variables for the server
-team_1 = ""
-team_1_connection = None
-team_2 = ""
-team_2_connection = None
-question = []
-answer = ''
-answer_team = -1
-lock1 = threading.Lock()
-lock2 = threading.Lock()
-question_bank = [["2+2","4"],["(5*2*7+8)*(5*0*5)+3","3"],["(6^2)/4","9"],["7+6+5-10","8"],["6*5*2/12","5"],["(17+18)/5","7"],["5!/20","6"],["2+6+1","9"],["(7+5*6+3)/5","8"],
- ["(2^5+18)/10","5"],["1+1","2"],["7*7/7","7"],["3^4/9","9"],["4^2-7","9"],["10*9*8*7*0*4","0"],["500/25/5","4"],["69*2/6-15","8"],["6*5/3-1","9"],["1024/256","4"],["7+10-8-6","3"]]
-BroadcastIP = '255.255.255.255'
-BroadcastPort = 13117
-buff_size = 1024
-
-# Press the green button in the gutter to run the script.
+UDPPort = 13117
+buf_size = 1024
 
 
-def clientThread(connection):
-
-    #getting the client's name. on a timer of 10 seconds
-    name, name_received = getTeamName(connection)
-
-    if name_received:
-        setTeamName(name, connection)
-    return
-
-
-
-def getClientAnswer(connection, client_num):
-    global answer,answer_team
-    print("client", client_num, " is answering")
-    message = ("you have 10 seconds to answer the following question:\n" + question[0])
+# thread that sends packets to the server
+def Start_Game(socket):
     try:
-        connection.sendall(message.encode('utf-8'))
-    except:
-        print(f"{Yellow}connection from client lost{End}")
-        try:
-            connection.close()
-            return
-        except:
-            return
-    end = time.time() + 10
-    while time.time() < end:
-        try:
-            data = connection.recv(1)
-            if data:
-                processed_data = data.decode('utf-8')
-                lock2.acquire()
-                print("locked")
-                if answer_team < 0:
-                    answer = processed_data
-                    answer_team = client_num
-                    print(answer, "\t", answer_team)
-                    lock2.release()
-                else:
-                    lock2.release()
-        except:
-            pass
-    try:
-        print("before")
-        connection.sendall(conclude().encode('utf-8'))
-        print("after")
-        try:
-            connection.close()
-        except:
-            return
-    except:
-        try:
-            connection.close()
-        except:
-            return
-
-
-
-
-def setTeamName(name, connection):
-    lock1.acquire()
-    global team_1, team_2, team_1_connection,team_2_connection
-    if len(team_1) == 0:
-        team_1 = name
-        team_1_connection = connection
-        lock1.release()
-        return
-    else:
-        team_2 = name
-        team_2_connection = connection
-        lock1.release()
-        return
-
-
-
-
-def getTeamName(connection):
-    name = ""
-    name_received = False
-    end = time.time() + 10
-    while time.time() < end and not name_received:
-        try:
-            data = connection.recv(1)
-            if data.decode('utf-8') == '\n':
-                name_received = True
-            else:
-                name = name + data.decode('utf-8')
-        except:
-            name_received = False
-    print(name)
-    return name, name_received
-
-def UDPInitConnection():
-    cs = socket(AF_INET, SOCK_DGRAM)
-    cs.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-    cs.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
-    return cs
-
-
-def TCPInitConnection(port):
-    host = gethostname()
-    sock = socket(AF_INET, SOCK_STREAM)
-    server_address = ("127.0.0.1", port)
-    try:
-        sock.bind(server_address)
-    except:
-        print(f"{Red}error binding{End}")
-    return sock
-
-def Main():
-    global team_1,team_2,team_1_connection,team_2_connection,question,answer,answer_team
-    host = gethostname()
-    port = random.randint(2000,40000)
-    print(port)
-    print(f"{Blue}server started, listening on IP address\n{End}",gethostbyname(host))
-
-    sock = TCPInitConnection(port)
-
-    cs = UDPInitConnection()
-    msg = pack('!IBH', 0xfeedbeef, 0x2, port)
-
-
-    try:
-        sock.listen()
-        counter = 0
-        client_threads = []
-        while True:
-            if(counter < 2):
-                try:
-                    cs.sendto(msg, (BroadcastIP, BroadcastPort))
-                except:
-                    print(f"{Red}broadcast failed{End}")
-                time.sleep(1)
-                sock.settimeout(0)
-                try:
-                    connection, addr = sock.accept()
-                    print("client has connected")
-                    connection.settimeout(0)
-                    t = threading.Thread(target= clientThread, args=(connection,))
-                    client_threads.append(t)
-                    counter = counter + 1
-                except:
-                    pass
-            elif (counter == 2):
-                n = random.randint(0,len(question_bank))
-                question = question_bank[n]
-                print(question)
-                for ct in client_threads:
-                    ct.start()
-                for ct in client_threads:
-                    ct.join()
-                t1 = threading.Thread(target=getClientAnswer, args=(team_1_connection, 1))
-                t2 = threading.Thread(target=getClientAnswer, args=(team_2_connection, 2))
-                t1.start()
-                t2.start()
-                t1.join()
-                t2.join()
-                print(conclude())
-                team_1 = ""
-                team_1_connection = None
-                team_2 = ""
-                team_2_connection = None
-                question = []
-                answer = ''
-                answer_team = -1
-                counter = 0
-                client_threads = []
+                #socket.settimeout(max(0, time_to_end - time_now))  # recive the max for timeout
+                send = getch.getch()  # receive the charactars as hiding chars , which dosent shown
+                print(send)
+                socket.sendall(send.encode('utf-8'))
+                print("was sent")
     except:
         pass
 
 
-def conclude():
-    to_return = ""
-    to_return += ("the correct answer was - " + question[1] + "\n")
-    if answer_team == -1:
-        to_return += "It's a Draw!"
-    else:
-        if answer == question[1]:
-            if answer_team == 1:
-                to_return += ("team " + team_1 + " is victorious!")
-            else:
-                to_return += ("team " + team_2 + " is victorious!")
-        else:
-            if answer_team == 1:
-                to_return += ("team " + team_2 + " is victorious!")
-            else:
-                to_return += ("team " + team_1 + " is victorious!")
-    return to_return
+# thread that listen to the server in the game so he can print the result of the game
+def Print_the_Score_Res(sock):
+    try:
+        time_now = time.time()
+        time_to_end = time.time() + 10
+        while time_now < time_to_end:
+            output = sock.recv(buf_size)
+            if output:
+                print(output.decode('utf-8'))
+            time_now = time.time()
+    except:
+        pass
+
+
+def Main():
+    name_of_team = "Beholder\n"
+    print(f"{Blue}Client started,listening for offer requests...\n{End}")
+    # initial the udp connection
+    cl = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    cl.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    cl.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    try:
+        cl.bind(('', UDPPort))
+    except:
+        print(f"{Red}error binding{End}")
+
+    while True:
+        # recivind the
+        data1, addr = cl.recvfrom(buf_size)
+        host, UDP_Port = addr
+        try:
+            data1, data2, TCP_Port = struct.unpack('!IBH', data1)
+            
+
+            
+            if data2 == 0x2 and data1 == 0xabcddcba:  # checking recieved message from broadcast
+                print(f"{Blue}received offer from{End}", host, f"{Blue},attempting to connect...\n{End}")
+                sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                server_address = (addr[0], TCP_Port)
+                try:
+                    sock.connect(server_address)
+                except:
+                    print(f"{Red}connection failed{End}")
+                try:
+                    print("sending the name")
+                    # sending the team name
+                    sock.sendall(name_of_team.encode('utf-8'))
+                    is_data_found = False
+                    time_to_end = time.time() + 10
+                    time_now = time.time()
+                    # give the server 10 sec to give us the permition to start the game
+                    while not is_data_found and time_now < time_to_end:
+                        data = sock.recv(2048)
+                        # starting the game
+                        if data is not None:
+                            print(data.decode('utf-8'))
+                            # open two threads on for sending, the other for lisining for the result of the end of the game
+                            # starting the first thread to send packets for 10 sec
+                            send = Thread(target=Start_Game, args=(sock,))
+                            send.start()
+                            send.join()
+                            # starting  the second thread which prints the server responses for 10 sec
+                            listen = Thread(target=Print_the_Score_Res, args=(sock,))
+                            listen.start()
+                            listen.join()
+                            is_data_found = True
+                        time_now = time.time()
+                except:
+                    print(f"{Red}server closed{End}")
+                finally:
+                    sock.close()
+        except:
+            pass
 
 
 if __name__ == '__main__':
     Main()
-
